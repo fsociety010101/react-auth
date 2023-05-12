52ce0276ddb60e828f361f70132e68b343aef8e5
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-05-12 23:30:09 +0200

    added auth funcionality

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
+import { AuthProvider } from './context/AuthProvider';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
